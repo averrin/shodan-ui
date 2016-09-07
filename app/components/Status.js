5f4826290d46a48b254dbd18a54ad81c571ef530
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2016-09-07 16:30:24 +0300

    playing with fonts

--- app/components/Status.js
+++ app/components/Status.js
@@ -1,15 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import Paper from 'material-ui/Paper';
+import FontIcon from 'material-ui/FontIcon';
+import { red500, green500 } from 'material-ui/styles/colors';
+import moment from 'moment';
+import IconButton from 'material-ui/IconButton';
 import styles from './Status.css';
-
-const style = {
-  height: 50,
-  width: 140,
-  margin: 20,
-  textAlign: 'left',
-  padding: 15,
-  display: 'inline-block',
-};
 
 class Status extends Component {
   static propTypes = {
@@ -24,9 +19,21 @@ class Status extends Component {
     const { online, lastSeen } = this.props.component;
     const component = this.props.name;
     return (
-      <Paper style={style} zDepth={1}>
-        <div className={`${styles.status}`} title={`Last seen: ${lastSeen}`}>
-          {component}: {online ? 'Online' : 'Offline'}
+      <Paper className={styles.status} zDepth={1}>
+        <div className={styles.wrapper}>
+          <div className={styles.iconContainer}>
+            <IconButton tooltip={`Last seen: ${moment(lastSeen).format('DD.MM HH:mm')}`}>
+              <FontIcon
+                className="material-icons"
+                color={online ? green500 : red500}
+              >
+                {online ? 'check' : 'error'}
+              </FontIcon>
+            </IconButton>
+          </div>
+          <div className={styles.nameContainer}>
+            {component}
+          </div>
         </div>
       </Paper>
     );
