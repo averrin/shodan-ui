00df1647e1d1e74680e965001198d67c6036a6a6
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2016-09-13 19:52:07 +0300

    usd

--- app/components/Amount/Amount.js
+++ app/components/Amount/Amount.js
@@ -5,11 +5,39 @@ export default class Amount extends Component {
   static propTypes = {
     value: PropTypes.number,
   }
+  componentWillMount() {
+    fetch('http://api.fixer.io/latest?base=rub').then(d => {
+      if (d.status === 200) {
+        return d.json();
+      }
+    }).then(rates => {
+      this.setState({ rates });
+    });
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rates: null
+    };
+  }
+
   render() {
+    let usd = '';
+    if (this.state.rates !== null) {
+      usd = (
+        <div className={styles.amount}>
+          Amount: {(this.props.value * this.state.rates.rates.USD).toFixed(2)}
+          <span style={{ color: '#555', fontSize: '10pt' }}>$</span>
+        </div>
+      );
+    }
     return (
-      <span className={styles.amount}>Amount: {this.props.value}
-        <span style={{ color: '#555', fontSize: '10pt' }}>₽</span>
-      </span>
+      <div>
+        <div className={styles.amount}>Amount: {this.props.value}
+          <span style={{ color: '#555', fontSize: '10pt' }}>₽</span>
+        </div>
+        {usd}
+      </div>
     );
   }
 }
